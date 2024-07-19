ea13f7506d884e30052ae3826d6e6cf517ff6a59
BHarish07 <harishbalike1995@gmail.com>
2024-07-19 14:58:39 +0530

    eks

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "devops-pratice-remote-state"
+    key    = "expense-dev-vpc"
+    region = "us-east-1"
+    dynamodb_table = "expense-dev-k8-ws"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
